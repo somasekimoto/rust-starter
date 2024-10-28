df6667cdf9a6839d849240fcc58a21de807f1e24
somasekimoto <0421.soma@gmail.com>
2024-10-28 20:44:14 +0900

    Add number parsing and basic arithmetic operations

--- chap3/src/main.rs
+++ chap3/src/main.rs
@@ -14,4 +14,37 @@ fn main() {
     let spaces = spaces.len();
 
     println!("The value of spaces is: {}", spaces);
+
+    let guess: u32 = "42".parse().expect("Not a number!");
+
+    let x = 2.0; // f64
+
+    let y: f32 = 3.0; // f32
+
+    // addition
+    // 足し算
+    let sum = 5 + 10;
+
+    // subtraction
+    // 引き算
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    // 掛け算
+    let product = 4 * 30;
+
+    // division
+    // 割り算
+    let quotient = 56.7 / 32.2;
+    let floored = 2 / 3; // Results in 0
+                         // 結果は0
+
+    // remainder
+    // 余り
+    let remainder = 43 % 5;
+
+    let t = true;
+
+    let f: bool = false; // with explicit type annotation
+                         // 明示的型注釈付きで
 }
