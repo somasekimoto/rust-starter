8f8df5f30e86964362d3968da3c16e92ad551eef
somasekimoto <0421.soma@gmail.com>
2024-10-28 20:35:54 +0900

    variables & shadowing

--- chap3/src/main.rs
+++ chap3/src/main.rs
@@ -1,6 +1,17 @@
 fn main() {
-    let mut x = 5;
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x: i32 = x * 2;
+        println!("The value of x in the inner scope is: {}", x);
+    }
+
     println!("The value of x is: {}", x);
-    x = 6;
-    println!("The value of x is: {}", x);
+
+    let spaces = "   ";
+    let spaces = spaces.len();
+
+    println!("The value of spaces is: {}", spaces);
 }
