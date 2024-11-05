c4c1582c7be9947ff8cb965d906040b23c704088
somasekimoto <0421.soma@gmail.com>
2024-11-06 06:18:06 +0900

    Add Message enum variants and method call implementation

--- chap6/src/main.rs
+++ chap6/src/main.rs
@@ -4,6 +4,28 @@ enum IpAddr {
     V6(String),
 }
 
+#[derive(Debug)]
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+struct QuitMessage; // ユニット構造体
+struct MoveMessage {
+    x: i32,
+    y: i32,
+}
+struct WriteMessage(String); // タプル構造体
+struct ChangeColorMessage(i32, i32, i32); // タプル構造体
+
+impl Message {
+    fn call(&self) {
+        // method body would be defined here
+        // メソッド本体はここに定義される
+    }
+}
 
 
 fn main() {
@@ -12,5 +34,12 @@ fn main() {
     let loopback = IpAddr::V6(String::from("::1"));
 
     println!("home: {:?}", home);
+
     println!("loopback: {:?}", loopback);
+
+    let m = Message::Write(String::from("hello"));
+
+    m.call();
+
+
 }
