ee5db9594739d61d5538c18876ddc5cf2583dc13
somasekimoto <0421.soma@gmail.com>
2024-11-06 05:29:48 +0900

    Add methods area() and can_hold() to Rectangle struct

--- chap5/src/main.rs
+++ chap5/src/main.rs
@@ -11,6 +11,16 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 fn main() {
     let mut user1 = User {
         email: String::from("someone@example.com"),
@@ -50,13 +60,23 @@ fn main() {
     let rect1 = Rectangle { width: 30, height: 50 };
 
     println!("rect1 is {:#?}", rect1);
-
-
     println!(
         "The area of the rectangle is {} square pixels.",
         area(&rect1)
     );
 
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+
+    let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
+
+    // rect1にrect2ははまり込む？
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
 
 fn build_user(email: String, username: String) -> User {
