efb2933101eef110e27a0280955c269177d3ad8f
somasekimoto <0421.soma@gmail.com>
2024-11-06 06:28:16 +0900

    Add pattern matching examples in main function

--- chap6/src/main.rs
+++ chap6/src/main.rs
@@ -88,6 +88,30 @@ fn main() {
         _ => (),
     }
 
+    let some_u8_value = Some(0u8);
+    match some_u8_value {
+        Some(3) => println!("three"),
+        _ => (),
+    }
+
+    if let Some(3) = some_u8_value {
+        println!("three");
+    }
+
+    let mut count = 0;
+    let coin = Coin::Quarter(UsState::Alaska);
+    match coin {
+        Coin::Quarter(state) => println!("State quarter from {:?}!", state),
+        _ => count += 1,
+    }
+
+    let coin = Coin::Quarter(UsState::Alaska);
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}!", state);
+    } else {
+        count += 1;
+    }
+
 }
 
 
