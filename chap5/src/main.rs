88f50fa59c1d5edf15a2e901c436af37aff04deb
somasekimoto <0421.soma@gmail.com>
2024-11-06 05:14:27 +0900

    Add User struct and build_user function

--- chap5/src/main.rs
+++ chap5/src/main.rs
@@ -1,1 +1,52 @@
-fn main() {}
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+fn main() {
+    let mut user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    println!("user1.email: {}", user1.email);
+
+    user1.email = String::from("anotheremail@example.com");
+    println!("user1.email: {}", user1.email);
+
+    let user1 = build_user(user1.email, user1.username);
+    println!("user1.email: {}", user1.email);
+
+
+    let user2 = User {
+        email: String::from("another@example.com"),
+        username: String::from("anotherusername567"),
+        ..user1
+    };
+
+    println!("user2.email: {}", user2.email);
+
+
+    struct Color(i32, i32, i32);
+    struct Point(i32, i32, i32);
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+
+    println!("black: {}, {}, {}", black.0, black.1, black.2);
+    println!("origin: {}, {}, {}", origin.0, origin.1, origin.2);
+
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
