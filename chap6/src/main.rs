1b9a0b2dd171b2a4b3ccd389fed32a118f6870ec
somasekimoto <0421.soma@gmail.com>
2024-11-06 05:48:24 +0900

    Add IpAddr struct and IpAddrKind enum with debug output

--- chap6/src/main.rs
+++ chap6/src/main.rs
@@ -1,1 +1,33 @@
-fn main() {}
+#[derive(Debug)]
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+#[derive(Debug)]
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+fn main() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    println!("four: {:?}", four);
+    println!("six: {:?}", six);
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    println!("home: {:?}", home);
+    println!("loopback: {:?}", loopback);
+}
