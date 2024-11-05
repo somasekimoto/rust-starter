41d968c3da76f21c6150e9598f518a9f40a5b7bc
somasekimoto <0421.soma@gmail.com>
2024-11-06 06:26:12 +0900

    Add plus_one function to increase Option<i32> by 1

--- chap6/src/main.rs
+++ chap6/src/main.rs
@@ -70,6 +70,24 @@ fn main() {
     let coin = Coin::Quarter(UsState::Alaska);
     let value = value_in_cents(coin);
     println!("value: {}", value);
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    println!("five: {:?}", five);
+    println!("six: {:?}", six);
+    println!("none: {:?}", none);
+
+    let some_u8_value = 0u8;
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (),
+    }
+
 }
 
 
@@ -85,5 +103,12 @@ fn value_in_cents(coin: Coin) -> u32 {
             println!("State quarter from {:?}!", state);
             25
         },
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
     }
 }
