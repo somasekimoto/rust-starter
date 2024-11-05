5998c05a38c8b6b01a184ed38cd61c54b0736997
somasekimoto <0421.soma@gmail.com>
2024-11-06 05:53:28 +0900

    Refactor IpAddrKind to IpAddr enum with associated data

--- chap6/src/main.rs
+++ chap6/src/main.rs
@@ -1,32 +1,15 @@
 #[derive(Debug)]
-enum IpAddrKind {
-    V4,
-    V6,
+enum IpAddr {
+    V4(String),
+    V6(String),
 }
 
-#[derive(Debug)]
 
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
-}
 
 fn main() {
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
 
-    println!("four: {:?}", four);
-    println!("six: {:?}", six);
-
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
-
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
+    let home = IpAddr::V4(String::from("127.0.0.1"));
+    let loopback = IpAddr::V6(String::from("::1"));
 
     println!("home: {:?}", home);
     println!("loopback: {:?}", loopback);
