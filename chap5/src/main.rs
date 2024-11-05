027d012248f6e5ba98b64b2cd2e46b83b68d3eae
somasekimoto <0421.soma@gmail.com>
2024-11-06 05:17:52 +0900

    Refactor area function to use tuple for dimensions

--- chap5/src/main.rs
+++ chap5/src/main.rs
@@ -41,14 +41,11 @@ fn main() {
     println!("origin: {}, {}, {}", origin.0, origin.1, origin.2);
 
     // rectangle
-
-    let width1 = 30;
-    let height1 = 50;
+    let rect1 = (30, 50);
 
     println!(
-        // 長方形の面積は、{}平方ピクセルです
         "The area of the rectangle is {} square pixels.",
-        area(width1, height1)
+        area(rect1)
     );
 
 }
@@ -62,6 +59,6 @@ fn build_user(email: String, username: String) -> User {
     }
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
