10a515c8704cb187c829c86ddf4e5f79376e1586
somasekimoto <0421.soma@gmail.com>
2024-11-06 06:06:47 +0900

    Update IP address V4 variant to use separate u8 values

--- chap6/src/main.rs
+++ chap6/src/main.rs
@@ -1,6 +1,6 @@
 #[derive(Debug)]
 enum IpAddr {
-    V4(String),
+    V4(u8, u8, u8, u8),
     V6(String),
 }
 
@@ -8,7 +8,7 @@ enum IpAddr {
 
 fn main() {
 
-    let home = IpAddr::V4(String::from("127.0.0.1"));
+    let home = IpAddr::V4(127, 0, 0, 1);
     let loopback = IpAddr::V6(String::from("::1"));
 
     println!("home: {:?}", home);
