367a8e248adcdd3b5568a8bb372b848f8b7d36d8
somasekimoto <0421.soma@gmail.com>
2024-11-10 18:00:25 +0900

    Add summer breakfast order functionality in lib.rs

--- chap7/restaurant/src/lib.rs
+++ chap7/restaurant/src/lib.rs
@@ -23,11 +23,41 @@ pub fn eat_at_restaurant() {
     // Relative path
     // 相対パス
     front_of_house::hosting::add_to_waitlist();
+
+    // Order a breakfast in the summer with Rye toast
+    // 夏 (summer) にライ麦 (Rye) パン付き朝食を注文
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    // やっぱり別のパンにする
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal
+    // 下の行のコメントを外すとコンパイルできない。食事についてくる
+    // 季節のフルーツを知ることも修正することも許されていないので
+    // meal.seasonal_fruit = String::from("blueberries");
 }
+
+
 
 fn serve_order() {}
 
 mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
     fn fix_incorrect_order() {
         cook_order();
         super::serve_order();
