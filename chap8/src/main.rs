1e3ee70a54fce1f65e335987f9283fef621fd1cd
somasekimoto <0421.soma@gmail.com>
2024-11-11 21:41:15 +0900

    Add enum SpreadsheetCell and create row vector

--- chap8/src/main.rs
+++ chap8/src/main.rs
@@ -42,4 +42,16 @@ fn main() {
         *i += 50;
     }
 
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+
 }
