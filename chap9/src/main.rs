7c0b8219b959f6d832e533f5cc5418b19a41d278
somasekimoto <0421.soma@gmail.com>
2024-11-12 00:09:42 +0900

    Add handling for file not found error when opening file

--- chap9/src/main.rs
+++ chap9/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs::File;
+use std::io::ErrorKind;
 
 fn main() {
     let v = vec![1, 2, 3];
@@ -10,9 +11,23 @@ fn main() {
 
     let f = match f {
         Ok(file) => file,
+        Err(ref error) if error.kind() == ErrorKind::NotFound => {
+            match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => {
+                    panic!(
+                        //ファイルを作成しようとしましたが、問題がありました
+                        "Tried to create file but there was a problem: {:?}",
+                        e
+                    )
+                },
+            }
+        },
         Err(error) => {
-            // ファイルを開く際に問題がありました
-            panic!("There was a problem opening the file: {:?}", error)
+            panic!(
+                "There was a problem opening the file: {:?}",
+                error
+            )
         },
     };
 }
