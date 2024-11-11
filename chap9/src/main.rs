146f443880a613be7f170d9c40da0022b4e92d08
somasekimoto <0421.soma@gmail.com>
2024-11-12 00:20:03 +0900

    Add file creation and read function in main.rs

--- chap9/src/main.rs
+++ chap9/src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+use std::io::Read;
 use std::fs::File;
 use std::io::ErrorKind;
 
@@ -9,28 +11,53 @@ fn main() {
 
     let f = File::open("hello.txt");
 
-    // let f = match f {
-    //     Ok(file) => file,
-    //     Err(ref error) if error.kind() == ErrorKind::NotFound => {
-    //         match File::create("hello.txt") {
-    //             Ok(fc) => fc,
-    //             Err(e) => {
-    //                 panic!(
-    //                     //ファイルを作成しようとしましたが、問題がありました
-    //                     "Tried to create file but there was a problem: {:?}",
-    //                     e
-    //                 )
-    //             },
-    //         }
-    //     },
-    //     Err(error) => {
-    //         panic!(
-    //             "There was a problem opening the file: {:?}",
-    //             error
-    //         )
-    //     },
-    // };
+    let f = match f {
+        Ok(file) => file,
+        Err(ref error) if error.kind() == ErrorKind::NotFound => {
+            match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => {
+                    panic!(
+                        //ファイルを作成しようとしましたが、問題がありました
+                        "Tried to create file but there was a problem: {:?}",
+                        e
+                    )
+                },
+            }
+        },
+        Err(error) => {
+            panic!(
+                "There was a problem opening the file: {:?}",
+                error
+            )
+        },
+    };
 
     // let f = File::open("hello.txt").unwrap();
-    let f = File::open("hello.txt").expect("Failed to open hello.txt");
+    // let f = File::open("hello.txt").expect("Failed to open hello.txt");
+
+    let f = read_username_from_file();
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => panic!("There was a problem opening the file: {:?}", error),
+    };
+    println!("f: {}", f);
+
+
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
 }
