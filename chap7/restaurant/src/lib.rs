9ff0c5afd4ae83a719d049a81ab22c3a3a7f1791
somasekimoto <0421.soma@gmail.com>
2024-11-11 20:48:24 +0900

    Refactor import statements for Result and IoResult

--- chap7/restaurant/src/lib.rs
+++ chap7/restaurant/src/lib.rs
@@ -46,8 +46,7 @@ pub fn eat_at_restaurant() {
     hosting::add_to_waitlist();
     hosting::add_to_waitlist();
 
-    use std::fmt::Result;
-    use std::io::Result as IoResult;
+    use std::{fmt::Result, io::Result as IoResult};
 
     fn function1() -> Result {
         return Ok(());
