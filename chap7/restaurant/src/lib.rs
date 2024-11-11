594b047fc421a315a5cd173ea3ebcea014f33d3e
somasekimoto <0421.soma@gmail.com>
2024-11-11 20:52:57 +0900

    Add front_of_house module with hosting and serving features

--- chap7/restaurant/src/lib.rs
+++ chap7/restaurant/src/lib.rs
@@ -1,18 +1,4 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
+mod front_of_house;
 
 pub use crate::front_of_house::hosting;
 
@@ -57,8 +43,6 @@ pub fn eat_at_restaurant() {
     }
 
 }
-
-
 
 fn serve_order() {}
 
