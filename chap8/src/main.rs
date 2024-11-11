1625aade6723a6fc6171673e66d5cf85e98deb6e
somasekimoto <0421.soma@gmail.com>
2024-11-11 21:08:00 +0900

    Add Cargo.toml and main.rs files for chap8 module

---
+++ chap8/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let v: Vec<i32> = Vec::new();
+    let v = vec![1, 2, 3];
+
+    println!("v: {:?}", v);
+
+    let mut v = Vec::new();
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
+
+    println!("v: {:?}", v);
+}
