4ccf8f18a2fc9a5d3006360837cb5be045caaaa0
somasekimoto <0421.soma@gmail.com>
2024-11-11 22:04:35 +0900

    Refactor string concatenation to use format! macro

--- chap8/src/main.rs
+++ chap8/src/main.rs
@@ -85,4 +85,19 @@ fn main() {
 
     println!("s3: {}", s3);
 
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = s1 + "-" + &s2 + "-" + &s3;
+    println!("s: {}", s);
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = format!("{}-{}-{}", s1, s2, s3);
+
+
+
 }
