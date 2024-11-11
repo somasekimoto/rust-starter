34fbfe0cad7908fc103790b8b1b6e5ad2f4dcc1f
somasekimoto <0421.soma@gmail.com>
2024-11-12 00:06:43 +0900

    Add file error handling in main function

--- chap9/src/main.rs
+++ chap9/src/main.rs
@@ -1,5 +1,18 @@
+use std::fs::File;
+
 fn main() {
     let v = vec![1, 2, 3];
 
-    v[99];
+    // v[99];
+
+
+    let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => {
+            // ファイルを開く際に問題がありました
+            panic!("There was a problem opening the file: {:?}", error)
+        },
+    };
 }
