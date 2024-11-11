3fc08c46f1180fe429146ea1c82d4b9516b9bacc
somasekimoto <0421.soma@gmail.com>
2024-11-11 22:30:33 +0900

    Add word count functionality to main function

--- chap8/src/main.rs
+++ chap8/src/main.rs
@@ -175,4 +175,14 @@ fn main() {
 
     println!("{:?}", scores);
 
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
 }
