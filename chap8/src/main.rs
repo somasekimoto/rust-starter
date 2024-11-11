f11656cbcd4aed75d8ab2d8daf0139d00636ab62
somasekimoto <0421.soma@gmail.com>
2024-11-11 22:19:34 +0900

    Add HashMap data structure to store and display scores

--- chap8/src/main.rs
+++ chap8/src/main.rs
@@ -118,4 +118,21 @@ fn main() {
         println!("{}", b);
     }
 
+    use std::collections::HashMap;
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    println!("scores: {:?}", scores);
+
+
+    let teams  = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+
+    let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+
+    println!("scores: {:?}", scores);
+
 }
