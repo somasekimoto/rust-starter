bb83cb5abeb53b1b80f4228e312048dc9104a1f1
somasekimoto <0421.soma@gmail.com>
2024-11-11 22:25:24 +0900

    Add hashmap example to main function

--- chap8/src/main.rs
+++ chap8/src/main.rs
@@ -144,4 +144,22 @@ fn main() {
     // field_name and field_value are invalid at this point, try using them and
     // see what compiler error you get!
 
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name);
+
+    match score {
+        Some(value) => println!("value: {}", value),
+        None => println!("None"),
+    }
+
+    for (key, value) in &scores {
+        println!("{}: {}", key, value);
+    }
+
+
 }
