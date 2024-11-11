99a3025815609b1726a34d5144f52a570d423423
somasekimoto <0421.soma@gmail.com>
2024-11-11 22:02:42 +0900

    Add string manipulation examples in main function

--- chap8/src/main.rs
+++ chap8/src/main.rs
@@ -54,4 +54,35 @@ fn main() {
         SpreadsheetCell::Float(10.12),
     ];
 
+
+    let mut s = String::new();
+
+    let data = "initial contents";
+
+    let s = data.to_string();
+
+    // the method also works on a literal directly:
+    let s = "initial contents".to_string();
+    println!("s: {}", s);
+
+    let mut s = String::from("foo");
+    s.push_str("bar");
+
+    println!("s: {}", s);
+
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    println!("s2 is {}", s2);
+
+    let mut s = String::from("lo");
+    s.push('l');
+    println!("s: {}", s);
+
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2; // s1はムーブされ、もう使用できないことに注意
+
+    println!("s3: {}", s3);
+
 }
