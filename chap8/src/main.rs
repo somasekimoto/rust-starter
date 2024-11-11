dcab3b51309272cd239270e29c1d4315d1637be3
somasekimoto <0421.soma@gmail.com>
2024-11-11 22:24:06 +0900

    Add HashMap entry for favorite color field

--- chap8/src/main.rs
+++ chap8/src/main.rs
@@ -135,4 +135,13 @@ fn main() {
 
     println!("scores: {:?}", scores);
 
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+    // println!("field_name: {}, field_value: {}", field_name, field_value);
+    // field_name and field_value are invalid at this point, try using them and
+    // see what compiler error you get!
+
 }
