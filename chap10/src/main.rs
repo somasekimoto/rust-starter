d7c5139e96e5a282b61e286c7d1e03f121831fbc
somasekimoto <0421.soma@gmail.com>
2024-11-12 21:13:00 +0900

    Add Summary trait and implement it for NewsArticle and Tweet

--- chap10/src/main.rs
+++ chap10/src/main.rs
@@ -34,6 +34,36 @@ enum Option_f64 {
     None,
 }
 
+pub trait Summary {
+    fn summarize(&self) -> String;
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
+
 fn main() {
     // let number_list = vec![34, 50, 25, 100, 65];
 
@@ -54,4 +84,16 @@ fn main() {
 
     let integer = Option_i32::Some(5);
     let float = Option_f64::Some(5.0);
+
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            // もちろん、ご存知かもしれませんがね、みなさん
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
 }
