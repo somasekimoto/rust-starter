12c3d2e2e4a91ab8b9d8fec8e8a59f56ab336355
somasekimoto <0421.soma@gmail.com>
2024-11-12 21:31:45 +0900

    Refactor largest function to use traits and generics

--- chap10/src/main.rs
+++ chap10/src/main.rs
@@ -1,14 +1,14 @@
-// fn largest<T>(list: &[T]) -> T {
-//     let mut largest = list[0];
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
 
-//     for &item in list.iter() {
-//         if item > largest {
-//             largest = item;
-//         }
-//     }
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
 
-//     largest
-// }
+    largest
+}
 
 struct Point<T, U> {
     x: T,
@@ -70,15 +70,15 @@ impl Summary for Tweet {
 }
 
 fn main() {
-    // let number_list = vec![34, 50, 25, 100, 65];
+    let number_list = vec![34, 50, 25, 100, 65];
 
-    // let result = largest(&number_list);
-    // println!("The largest number is {}", result);
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
 
-    // let char_list = vec!['y', 'm', 'a', 'q'];
+    let char_list = vec!['y', 'm', 'a', 'q'];
 
-    // let result = largest(&char_list);
-    // println!("The largest char is {}", result);
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
 
     let p1 = Point { x: 5, y: 10.4 };
     let p2 = Point { x: "Hello", y: 'c'};
