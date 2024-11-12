1e84ffba15c593a8ab701a9c6284093712286b02
somasekimoto <0421.soma@gmail.com>
2024-11-12 21:18:57 +0900

    Add method to summarize author in trait Summary

--- chap10/src/main.rs
+++ chap10/src/main.rs
@@ -35,9 +35,11 @@ enum Option_f64 {
 }
 
 pub trait Summary {
+    fn summarize_author(&self) -> String;
+
     fn summarize(&self) -> String {
-        // "（もっと読む）"
-        String::from("(Read more...)")
+        // "（{}さんの文章をもっと読む）"
+        format!("(Read more from {}...)", self.summarize_author())
     }
 }
 
@@ -48,11 +50,11 @@ pub struct NewsArticle {
     pub content: String,
 }
 
-impl Summary for NewsArticle {
-    fn summarize(&self) -> String {
-        format!("{}, by {} ({})", self.headline, self.author, self.location)
-    }
-}
+// impl Summary for NewsArticle {
+//     fn summarize(&self) -> String {
+//         format!("{}, by {} ({})", self.headline, self.author, self.location)
+//     }
+// }
 
 pub struct Tweet {
     pub username: String,
@@ -62,8 +64,8 @@ pub struct Tweet {
 }
 
 impl Summary for Tweet {
-    fn summarize(&self) -> String {
-        format!("{}: {}", self.username, self.content)
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
     }
 }
 
@@ -100,19 +102,31 @@ fn main() {
 
     println!("1 new tweet: {}", tweet.summarize());
 
-    let article = NewsArticle {
-        // ペンギンチームがスタンレーカップチャンピオンシップを勝ち取る！
-        headline: String::from("Penguins win the Stanley Cup Championship!"),
-        // アメリカ、ペンシルベニア州、ピッツバーグ
-        location: String::from("Pittsburgh, PA, USA"),
-        // アイスバーグ
-        author: String::from("Iceburgh"),
-        // ピッツバーグ・ペンギンが再度NHL(National Hockey League)で最強のホッケーチームになった
+    // let article = NewsArticle {
+    //     // ペンギンチームがスタンレーカップチャンピオンシップを勝ち取る！
+    //     headline: String::from("Penguins win the Stanley Cup Championship!"),
+    //     // アメリカ、ペンシルベニア州、ピッツバーグ
+    //     location: String::from("Pittsburgh, PA, USA"),
+    //     // アイスバーグ
+    //     author: String::from("Iceburgh"),
+    //     // ピッツバーグ・ペンギンが再度NHL(National Hockey League)で最強のホッケーチームになった
+    //     content: String::from(
+    //         "The Pittsburgh Penguins once again are the best \
+    //          hockey team in the NHL.",
+    //     ),
+    // };
+
+    // println!("New article available! {}", article.summarize());
+
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
         content: String::from(
-            "The Pittsburgh Penguins once again are the best \
-             hockey team in the NHL.",
+            "of course, as you probably already know, people",
         ),
+        reply: false,
+        retweet: false,
     };
 
-    println!("New article available! {}", article.summarize());
+    println!("1 new tweet: {}", tweet.summarize());
+
 }
