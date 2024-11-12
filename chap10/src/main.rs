8d54fb525108ddf595e635928c78a9757a828585
somasekimoto <0421.soma@gmail.com>
2024-11-12 21:38:32 +0900

    Add Pair struct with cmp_display method

--- chap10/src/main.rs
+++ chap10/src/main.rs
@@ -1,3 +1,4 @@
+
 fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
     let mut largest = list[0];
 
@@ -66,6 +67,29 @@ pub struct Tweet {
 impl Summary for Tweet {
     fn summarize_author(&self) -> String {
         format!("@{}", self.username)
+    }
+}
+
+use std::fmt::Display;
+
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
     }
 }
 
