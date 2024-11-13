cfeacfc08210053b20750567985a123ba8d25d70
somasekimoto <0421.soma@gmail.com>
2024-11-13 23:50:11 +0900

    Refactor Guess struct validation and test message

--- chap11/adder/src/lib.rs
+++ chap11/adder/src/lib.rs
@@ -20,9 +20,18 @@ pub struct Guess {
 
 impl Guess {
     pub fn new(value: i32) -> Guess {
-        if value < 1 || value > 100 {
-            //予想値は1から100の間でなければなりませんが、{}でした。
-            panic!("Guess value must be between 1 and 100, got {}.", value);
+        if value < 1 {
+            panic!(
+                //予想値は1以上でなければなりませんが、{}でした。
+                "Guess value must be greater than or equal to 1, got {}.",
+                value
+            );
+        } else if value > 100 {
+            panic!(
+                //予想値は100以下でなければなりませんが、{}でした。
+                "Guess value must be less than or equal to 100, got {}.",
+                value
+            );
         }
 
         Guess { value }
@@ -78,7 +87,8 @@ mod tests {
     }
 
     #[test]
-    #[should_panic]
+    //予想値は100以下でなければなりません
+    #[should_panic(expected = "Guess value must be less than or equal to 100")]
     fn greater_than_100() {
         Guess::new(200);
     }
