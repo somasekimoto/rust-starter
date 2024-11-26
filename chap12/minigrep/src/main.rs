d3097be8f54993a0c7bf61c6d503bfb5f1df0f0d
somasekimoto <0421.soma@gmail.com>
2024-11-27 03:02:14 +0900

    Refactor code to use a separate run function

--- chap12/minigrep/src/main.rs
+++ chap12/minigrep/src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,24 +13,23 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
-
-
-    let mut f = File::open(config.filename).expect("file not found");
-
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        // ファイルの読み込み中に問題がありました
-        .expect("something went wrong reading the file");
-
-    // テキストは\n{}です
-    println!("With text:\n{}", contents);
+    run(config);
 }
 
 struct Config {
     query: String,
     filename: String,
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let mut f = File::open(config.filename)?;
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)?;
+
+    println!("With text:\n{}", contents);
+
+    Ok(())
 }
 
 impl Config {
